e23370b736a3b6762643b607d6e8bdfedfaa8b0c
Rod Gorlabach <rodgorlabach@users.noreply.github.com>
2025-10-26 16:44:05 -0500

    Initialize HDC3022 component with imports
    Import necessary modules and define HDC3022 component.

--- components/hdc302x/__init__.py
+++ components/hdc302x/__init__.py
@@ -1,1 +1,9 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+# Define a namespace for our component
+hdc3022_ns = cg.esphome_ns.namespace("hdc3022")
+
+# Define the C++ component class
+HDC3022Component = hdc3022_ns.class_("HDC3022Component", cg.PollingComponent, cg.Component)
 
