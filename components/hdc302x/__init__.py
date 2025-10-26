139ac812ea135ed4532811831cb13437e78b25ba
Rod Gorlabach <rodgorlabach@users.noreply.github.com>
2025-10-26 16:46:36 -0500

    Rename component namespace and class for HDC302x

--- components/hdc302x/__init__.py
+++ components/hdc302x/__init__.py
@@ -2,8 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 
 # Define a namespace for our component
-hdc3022_ns = cg.esphome_ns.namespace("hdc3022")
+hdc302x_ns = cg.esphome_ns.namespace("hdc302x")
 
 # Define the C++ component class
-HDC3022Component = hdc3022_ns.class_("HDC3022Component", cg.PollingComponent, cg.Component)
+HDC302xComponent = hdc302x_ns.class_("HDC302xComponent", cg.PollingComponent, cg.Component)
 
