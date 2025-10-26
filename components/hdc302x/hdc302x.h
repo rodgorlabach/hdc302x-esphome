eea10d83493ea8d411a6a7702ab5c5c4a5fec47d
Rod Gorlabach <rodgorlabach@users.noreply.github.com>
2025-10-26 16:44:51 -0500

    Refactor HDC302XComponent to HDC3022Component

--- components/hdc302x/hdc302x.h
+++ components/hdc302x/hdc302x.h
@@ -1,34 +1,39 @@
-#pragma once
+#include "esphome.h"
 
-#include "esphome/components/i2c/i2c.h"
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/core/component.h"
-
-namespace esphome {
-namespace hdc302x {
-
-class HDC302XComponent : public PollingComponent, public i2c::I2CDevice {
+class HDC3022Component : public PollingComponent, public i2c::I2CDevice {
  public:
-  void set_temperature(sensor::Sensor *temperature) {
-    temperature_ = temperature;
+  Sensor *temperature_sensor = nullptr;
+  Sensor *humidity_sensor = nullptr;
+
+  void set_temperature_sensor(Sensor *sensor) { temperature_sensor = sensor; }
+  void set_humidity_sensor(Sensor *sensor) { humidity_sensor = sensor; }
+
+  HDC3022Component() : PollingComponent(5000) {}
+
+  void setup() override {
+    // Nothing special on setup
   }
 
-  void set_humidity(sensor::Sensor *humidity) { humidity_ = humidity; }
+  void update() override {
+    uint8_t command[2] = {0x24, 0x00};
+    write(command, 2);
+    delay(50);
 
-  void setup() override;
+    uint8_t data[6];
+    if (read(data, 6) != i2c::ERROR_OK) {
+      ESP_LOGW("hdc3022", "I2C read failed!");
+      return;
+    }
 
-  void dump_config() override;
+    uint16_t raw_temp = (data[0] << 8) | data[1];
+    uint16_t raw_hum = (data[3] << 8) | data[4];
 
-  void update() override;
+    float temperature = ((float)raw_temp * 165.0f / 65535.0f) - 40.0f;
+    float humidity = ((float)raw_hum * 100.0f / 65535.0f);
 
-  float get_setup_priority() const override;
-
-  bool read_data(uint16_t &temperature, uint16_t &humidity);
-
- protected:
-  sensor::Sensor *temperature_{nullptr};
-  sensor::Sensor *humidity_{nullptr};
+    if (temperature_sensor)
+      temperature_sensor->publish_state(temperature);
+    if (humidity_sensor)
+      humidity_sensor->publish_state(humidity);
+  }
 };
-
-}  // namespace hdc302x
-}  // namespace esphome
