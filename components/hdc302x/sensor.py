2e5fb0481aea6d05f047f14db4e3d4518ebeeffc
Rod Gorlabach <rodgorlabach@users.noreply.github.com>
2025-10-26 16:45:07 -0500

    Refactor HDC302X sensor for HDC3022 compatibility

--- components/hdc302x/sensor.py
+++ components/hdc302x/sensor.py
@@ -1,57 +1,44 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, sensor
+from esphome.components import sensor, i2c
 from esphome.const import (
-    CONF_ID,
-    CONF_TEMPERATURE,
-    CONF_HUMIDITY,
+    UNIT_CELSIUS,
+    UNIT_PERCENT,
     DEVICE_CLASS_TEMPERATURE,
     DEVICE_CLASS_HUMIDITY,
     STATE_CLASS_MEASUREMENT,
-    UNIT_CELSIUS,
-    UNIT_PERCENT,
-)
-CODEOWNERS = ["@HarpalPannu, @rodgorlabach"]
-DEPENDENCIES = ["i2c"]
-
-hdc302x_ns = cg.esphome_ns.namespace("hdc302x")
-HDC302XComponent = hdc302x_ns.class_(
-    "HDC302XComponent", sensor.Sensor, cg.PollingComponent, i2c.I2CDevice
 )
 
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(HDC302XComponent),
-            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
-                unit_of_measurement=UNIT_CELSIUS,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_TEMPERATURE,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(
-                unit_of_measurement=UNIT_PERCENT,
-                accuracy_decimals=0,
-                device_class=DEVICE_CLASS_HUMIDITY,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-        }
-    )
-    .extend(cv.polling_component_schema("1s"))
-    .extend(i2c.i2c_device_schema(0x44))
-)
+from . import hdc3022_ns, HDC3022Component
+
+CONF_TEMPERATURE = "temperature"
+CONF_HUMIDITY = "humidity"
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(HDC3022Component),
+    cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=2,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(
+        unit_of_measurement=UNIT_PERCENT,
+        accuracy_decimals=2,
+        device_class=DEVICE_CLASS_HUMIDITY,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+}).extend(i2c.i2c_device_schema(0x44)).extend(cv.polling_component_schema("5s"))
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[cg.CONF_ID])
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
     if CONF_TEMPERATURE in config:
         sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
-        cg.add(var.set_temperature(sens))
+        cg.add(var.set_temperature_sensor(sens))
 
     if CONF_HUMIDITY in config:
         sens = await sensor.new_sensor(config[CONF_HUMIDITY])
-
-        cg.add(var.set_humidity(sens))
-
+        cg.add(var.set_humidity_sensor(sens))
